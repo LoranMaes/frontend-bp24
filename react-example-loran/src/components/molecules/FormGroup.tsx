db328d73a8a711dcd4046ca9e84a7c7250356a62
Loran Maes <loran.maes@student.odisee.be>
2024-05-05 19:55:08 +0200

    React PWA start

---
+++ react-example-loran/src/components/molecules/FormGroup.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormField from "../atoms/FormField";
+
+export default function FormGroup() {
+  return (
+    <React.Fragment>
+      <label>FormGroup</label>
+      <FormField></FormField>
+    </React.Fragment>
+  );
+}
---
+++ react-example-loran/src/components/organisms/Form.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormGroup from "../molecules/FormGroup";
+
+export default function Form() {
+  return (
+    <form>
+      <FormGroup></FormGroup>
+      <FormGroup></FormGroup>
+      <FormGroup></FormGroup>
+    </form>
+  );
+}
