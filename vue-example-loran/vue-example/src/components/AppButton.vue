c978dcb2161c71b162950261a9d8f03c1b4b619a
Loran Maes <loran.maes@student.odisee.be>
2024-05-06 19:06:42 +0200

    Vue example + addes scss

---
+++ vue-example-loran/vue-example/src/components/AppButton.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+const buttonPress = defineModel('buttonPress', { required: true })
+</script>
+
+<template>
+  <button @click="buttonPress"><slot></slot></button>
+</template>
---
+++ vue-example-loran/vue-example/src/views/StateView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue'
+import { ref } from 'vue'
+
+const counter = ref(0)
+</script>
+
+<template>
+  <div class="items">
+    <h2>State</h2>
+    <p>Current button size: {{ counter }}</p>
+    <AppButton :button-press="() => (counter < 100 ? counter++ : null)">This is what comes in the slot</AppButton>
+    <span id="loader" :style="{ width: counter + '%' }"></span>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.items {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  #loader {
+    width: 0%;
+    height: 1rem;
+    background-color: pink;
+    border-radius: 0.5rem;
+    transition: width 250ms ease-in-out;
+  }
+}
+</style>
