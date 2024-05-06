6eb0212cd510168324800b16a2b50a0c47f1b866
Loran Maes <loran.maes@student.odisee.be>
2024-05-06 21:13:28 +0200

    Angular with conditional rendering and components

--- angular-example-loran/angular-example/src/app/app.component.ts
+++ angular-example-loran/angular-example/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './components/atoms/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-example';
+
+  counter = 0;
+
+  // Handle the event
+  clickHandler() {
+    this.counter < 100 ? this.counter++ : null;
+  }
 }
---
+++ angular-example-loran/angular-example/src/app/components/atoms/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss',
+})
+export class ButtonComponent {
+  // Output the click event
+  @Output() click = new EventEmitter<void>();
+}
